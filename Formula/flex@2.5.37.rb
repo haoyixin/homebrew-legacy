cd95154df5dfcc8e0661085ede6bda32d3df1f4b
Yixin Hao <yixin.hao@shopee.com>
2022-05-19 13:21:35 +0800

    Init commit.

---
+++ Formula/flex@2.5.37.rb
@@ -0,0 +1,36 @@
+class FlexAT2537 < Formula
+  homepage "http://flex.sourceforge.net"
+  url "https://downloads.sourceforge.net/flex/flex-2.5.37.tar.bz2"
+  #sha256 "db4b140f2aff34c6197cab919828cc4146aae218"
+
+  keg_only :provided_by_osx, "Some formulae require a newer version of flex."
+
+  depends_on "gettext"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.flex").write <<-EOS.undent
+      CHAR   [a-z][A-Z]
+      %%
+      {CHAR}+      printf("%s", yytext);
+      [ \\t\\n]+   printf("\\n");
+      %%
+      int main()
+      {
+        yyin = stdin;
+        yylex();
+      }
+    EOS
+    system "#{bin}/flex", "test.flex"
+    system ENV.cc, "lex.yy.c", "-L#{lib}", "-lfl", "-o", "test"
+    assert_equal shell_output("echo \"Hello World\" | ./test"), <<-EOS.undent
+      Hello
+      World
+    EOS
+  end
+end
